e7d6cb7de3b4d53040e472c411258a71eee84033
Omar <etrius460@gmail.com>
2026-06-11 15:58:30 +0300

    Handle user hyperlink format.

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ def msg_is_valid(message: str) -> bool:
         return True
 
     message_words = message.split()
-    first_link = [word.strip() for word in message_words if word.startswith("https")]
+    first_link = [word.strip() for word in message_words if word.startswith("https") or word.startswith("(https")]
 
     if not first_link:
         return False
