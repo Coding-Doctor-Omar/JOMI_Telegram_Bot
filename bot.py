be75cb8fb4e4b8676bd9742c5d73bd52b366a901
Omar <etrius460@gmail.com>
2026-06-11 16:14:24 +0300

    Handle user hyperlink format.

--- bot.py
+++ bot.py
@@ -177,7 +177,7 @@ def process_message():
             return "OK", 200
 
     else:
-        send_msg(chat_id=chat_id, msg="The message you sent me is not a valid command or a valid link for any JOMI video page. Please try again.")
+        send_msg(chat_id=chat_id, msg=f"The message you sent me is not a valid command or a valid link for any JOMI video page. Please try again.\n\nYour message: {user_message}")
         return "OK", 200
 
 
